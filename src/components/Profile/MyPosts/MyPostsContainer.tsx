0a0e0b3f74010eaaf57c198fb31dc8053b5d2bd0
slice of code <slice_of_code@mail.ru>
2024-02-05 18:37:14 +0400

    45

--- src/components/Profile/MyPosts/MyPostsContainer.tsx
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -2,8 +2,30 @@ import React from "react";
 import {addPostActionCreator, updatePostTextActionCreator} from "../../../Redux/profile-reducer";
 import {MyPosts} from "./MyPosts";
 import {StoreContext} from "../../../StoreContext";
+import {connect} from "react-redux";
+import {StateType, store} from "../../../Redux/store";
 
 export const MyPostsContainer = () => {
+
+    const mapStateToProps = (state: StateType) => {
+        return {
+            posts: state.profilePage.posts,
+            newPostText: state.profilePage.newPostText
+        }
+    }
+
+    const mapDispatchToProps = (dispatch: any) => {
+        return {
+            addPost: () => {
+                dispatch(addPostActionCreator(store._state.profilePage.newPostText))
+            },
+            onPostChange: (text: string) => {
+                dispatch(updatePostTextActionCreator(text))
+            }
+        }
+    }
+
+    const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps) (MyPosts)
 
     return (
         <StoreContext.Consumer>
