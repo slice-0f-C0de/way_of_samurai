3b3b9b9c5bf274b5b52ad6aae53924b68c42e2d7
slice of code <slice_of_code@mail.ru>
2024-02-15 12:28:21 +0400

    55

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -12,9 +12,14 @@ class Users extends React.Component<UsersPropsType, any> {
         )
     }
 
-
     render() {
         return <div>
+
+            <span>1</span>
+            <span>2</span>
+            <span>3</span>
+            <span>4</span>
+            <span>5</span>
 
             {this.props.users.users.map(u => <div key={u.id}>
             <span>
