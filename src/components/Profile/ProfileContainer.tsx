d781e7daa830c93658e300ddd1790fadeba4c139
slice of code <slice_of_code@mail.ru>
2024-02-17 00:01:04 +0400

    60

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -40,4 +40,4 @@ const WithUrlDataContainerComponent = withRouter(ProfileContainer)
 
 export default connect(mapStateToProps,{
     setUserProfileActionCreator
-}) (ProfileContainer)
+}) (WithUrlDataContainerComponent)
