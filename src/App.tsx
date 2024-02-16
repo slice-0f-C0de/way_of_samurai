6a70d862ad803b00f5cc996e72d73867c5a7c082
slice of code <slice_of_code@mail.ru>
2024-02-16 13:24:01 +0400

    59

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
-import {ProfileContainer} from "./components/Profile/ProfileContainer";
+import {Profile} from "./components/Profile/Profile";
 import {BrowserRouter, Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
@@ -15,7 +15,7 @@ const App: React.FC = () => {
                 <Navbar/>
                 <div className={"app-wrapper-content"}>
                     <Route path={"/profile"}
-                           render={() => <ProfileContainer />}/>
+                           render={() => <Profile />}/>
                     <Route path={"/dialogs"}
                            render={() => <DialogsContainer />}/>
                     <Route path={"/users"}
--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import c from "./Profile.module.css";
-import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import {MyPostsContainer} from "./MyPosts/MyPostsContainer";
+import {Profile} from "./Profile";
 
-export const ProfileContainer = () => {
-    return <div className={c.content}>
-            <ProfileInfo />
-        <MyPostsContainer />
-    </div>
+class ProfileContainer extends React.Component {
+    render() {
+        return <Profile/>
+    }
 }
+
+export default ProfileContainer
