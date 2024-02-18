83d8aa9835e0e9b84eea4efa0c4e9d12a7f57d00
slice of code <slice_of_code@mail.ru>
2024-02-18 16:11:25 +0400

    63

--- src/api.js
+++ src/api.js
@@ -1,25 +1,22 @@
 import axios from "axios";
 
-const baseUrl =  `https://social-network.samuraijs.com/api/1.0/`
+const instance = axios.create({
+        withCredentials: true,
+        baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+        header: {
+            'API-KEY': '58f98d8a-f688-42bf-8a92-24a3ecbcc273'
+        }
+    }
+)
 
 export const getUsers = (currentPage, pageSize) => {
-    return axios.get(baseUrl + `users?page=${currentPage}&count=${pageSize}`,
-        {
-            withCredentials: true
-        })
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
         .then(response => response.data)
 
 }
 
 export const authMe = () => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`,{
-        withCredentials: true
-    })
+    return instance.get(`auth/me`)
         .then(response => response.data)
 
 }
-
-// export const authProfile = (userId) => {
-//     return axios.get(baseUrl + `profile/` + userId)
-//         .then(response => response.data)
-// }
