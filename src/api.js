817cad757d3697d2ed3662cfd1df46562fefc2a0
slice of code <slice_of_code@mail.ru>
2024-02-24 17:42:04 +0400

    66

--- src/api.js
+++ src/api.js
@@ -23,11 +23,11 @@ export const usersAPI = {
     },
 
     follow(userId) {
-        return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`)
+        return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
     },
 
     unfollow(userId) {
-        return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`)
+        return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
     }
 }
 
