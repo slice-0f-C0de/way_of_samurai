f6100d5af74d72b113129c2ca4a8e2ea88c50064
slice of code <slice_of_code@mail.ru>
2024-03-18 23:53:20 +0400

    73

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -31,12 +31,20 @@ class ProfileStatus extends React.Component<ProfileStatusType, any> {
         })
     }
 
+    componentDidUpdate(prevProps: Readonly<ProfileStatusType>, prevState: Readonly<any>) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
     render() {
         return (
             <div>
                 {!this.state.editMode &&
                     <div>
-                        <span onDoubleClick={this.activateEditMode}>{this.state.status}</span>
+                        <span onDoubleClick={this.activateEditMode}>{this.state.status || '------'}</span>
                     </div>}
                 {this.state.editMode &&
                     <div>
