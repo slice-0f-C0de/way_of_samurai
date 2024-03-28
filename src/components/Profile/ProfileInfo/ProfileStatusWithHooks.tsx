a779d0f5f396cc59ad11b01809d3f39dd46d00de
slice of code <slice_of_code@mail.ru>
2024-03-28 19:18:36 +0400

    84

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -0,0 +1,37 @@
+import React, {ChangeEvent, useState} from 'react';
+
+type ProfileStatusType = {
+    status: string
+    updateStatus: (status: string) => void
+}
+
+export const ProfileStatusWithHooks = (props: ProfileStatusType) => {
+
+    const [editMode, setEditMode] = useState(false)
+    const [status, setStatus] = useState(props.status)
+
+    const activateEditMode = () => {
+        setEditMode(editMode)
+    }
+
+    const onStatusChanged = (e: ChangeEvent<HTMLInputElement>) => {
+        setStatus(e.currentTarget.value)
+    }
+
+    return (
+        <div>
+            {!editMode &&
+                <div>
+                    <span onDoubleClick={activateEditMode}>{props.status || '------'}</span>
+                </div>}
+            {editMode &&
+                <div>
+                    <input autoFocus={true} onChange={onStatusChanged} value={status}
+                           onBlur={activateEditMode}></input>
+                </div>
+            }
+        </div>
+    )
+}
+
+export default ProfileStatusWithHooks
